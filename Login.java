35e6da1cb0b5df753f7a3d38baef5c85347425f3
18244513 <18244513@studentmail.ul.ie>
2021-11-11 23:22:21 +0000

    Changed Login

--- Login.java
+++ Login.java
@@ -1,5 +1,12 @@
 package com.springbootattempt.springbootattempt1;
  
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import javax.persistence.*;
  
 @Entity
@@ -13,10 +20,30 @@ public class Login {
         super();
     }
  
-    public Login(long id, String password) {
+    public Login(long id, String password) throws IOException {
         super();
         this.id = id;
         this.password = password;
+        try{
+            ArrayList<String> ar = new ArrayList<String>();
+            File csvFile = new File("Attempt.csv");
+            BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            String line = "";
+            StringTokenizer st = null;
+            int lineNumber = 0;
+            int tokenNumber = 0;
+            while ((line = br.readLine()) != null) {
+                String[] arr = line.split(",");
+                System.out.println(arr[0]+" " + arr[1]);
+                lineNumber++;
+            }
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+
+        }
     }
  
     public long getId() {
