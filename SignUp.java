701ac7429c1395356c3325c4914d44a66e91d278
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2021-11-13 20:46:45 +0000

    changes

--- SignUp.java
+++ SignUp.java
@@ -1,42 +1,41 @@
 package com.springbootattempt.springbootattempt1;
  
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.persistence.*;
-import com.springbootattempt.springbootattempt1.CsvWriter;
  
 @Entity
 public class SignUp {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
-    public String firstname;
-    public String lastname;
+    public static String firstname = "g";
+    public static String lastname = "gjhgy";
     private String password;
-   
-    public SignUp() {
-        super();
-    }
- 
-    public SignUp(String firstname, String lastname, String password) {
-        super();
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.password = password;
-    }
- 
-    public long setId() {
+    
 
-        return id;
-    }
 
-    public void addFirstname(String firstname) {
-        
-    }
+    public static void main(String[] args) throws IOException {
+  
+        try (FileWriter writer = new FileWriter("test.csv", true)) {
+    
+          StringBuilder sb = new StringBuilder();
+          sb.append(firstname);
+          sb.append(',');
+          sb.append(lastname);
+          sb.append('\n');
+    
+          writer.write(sb.toString());
+    
+          System.out.println("done!");
+    
+        } catch (FileNotFoundException e) {
+          System.out.println(e.getMessage());
+        }
+    
+      }
 
-    public void addLastname(String lastname) {
-        
-    }
-
-    public void addPassword(String password) {
-        
-    }
+      
 }
