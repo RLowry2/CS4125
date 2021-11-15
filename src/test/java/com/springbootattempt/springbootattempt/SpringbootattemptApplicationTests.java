078b02485818801576d421598beb9a3374fc6669
lizunok-strn <lizozavr@gmail.com>
2021-11-15 16:37:58 +0000

    added admin/admiController/roomRepos [unfinished]

---
+++ src/test/java/com/springbootattempt/springbootattempt/SpringbootattemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootattempt.springbootattempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootattemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
