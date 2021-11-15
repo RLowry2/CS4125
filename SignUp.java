ba785dd54ed45931c35e1aa16c4bb500f6f53f46
LOWRY RIOGHAN <c18226531@cs304-08.CSIS>
2021-11-15 16:14:53 +0000

    sign up changes

--- SignUp.java
+++ SignUp.java
@@ -1,41 +1,48 @@
 package com.springbootattempt.springbootattempt1;
- 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javax.persistence.*;
- 
-@Entity
+
 public class SignUp {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
-    public static String firstname = "g";
-    public static String lastname = "gjhgy";
-    private String password;
-    
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private String firstname;
+  private String lastname;
+  private String password;
 
+  public SignUp() {
+    super();
+  }
 
-    public static void main(String[] args) throws IOException {
-  
-        try (FileWriter writer = new FileWriter("test.csv", true)) {
-    
-          StringBuilder sb = new StringBuilder();
-          sb.append(firstname);
-          sb.append(',');
-          sb.append(lastname);
-          sb.append('\n');
-    
-          writer.write(sb.toString());
-    
-          System.out.println("done!");
-    
-        } catch (FileNotFoundException e) {
-          System.out.println(e.getMessage());
-        }
-    
+  public SignUp(String firstname, String lastname, String password) throws IOException {
+    super();
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.password = password;
+
+    try {
+      ArrayList<String> ar = new ArrayList<String>();
+      File csvFile = new File("Attempt.csv");
+      BufferedReader br = new BufferedReader(new FileReader(csvFile));
+      String line = "";
+      StringTokenizer st = null;
+      int lineNumber = 0;
+      int tokenNumber = 0;
+      while ((line = br.readLine()) != null) {
+        String[] arr = line.split(",");
+        System.out.println(arr[0] + " " + arr[1]);
+        lineNumber++;
       }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    } finally {
 
-      
+    }
+  }
 }
