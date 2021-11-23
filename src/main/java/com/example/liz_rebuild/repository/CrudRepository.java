c2ba419e87d81f295fa9cd1fd1d5f490f9008eb2
lizunok-strn <lizozavr@gmail.com>
2021-11-23 21:22:07 +0000

    matching files to the main

---
+++ src/main/java/com/example/liz_rebuild/repository/CrudRepository.java
@@ -0,0 +1,2 @@
+package com.example.liz_rebuild.repository;public class CrudRepository {
+}
--- src/main/java/com/example/liz_rebuild/repository/UserRepository.java
+++ src/main/java/com/example/liz_rebuild/repository/UserRepository.java
@@ -1,16 +1,2 @@
-package com.example.liz_rebuild.repository;
- 
-import java.util.List;
-
-import com.example.liz_rebuild.model.User;
-import org.springframework.data.repository.CrudRepository;
- 
-public interface UserRepository extends CrudRepository<User, Long> {
- 
-    Object findOne(Long id);
- 
-    void save(List<User> users);
- 
+package com.example.liz_rebuild.serverlet;public class UserRepository {
 }
- 
-
