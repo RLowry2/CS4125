a8d2cd4a31f16db9be093ebadaaf2a84bb64c850
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2021-12-02 22:13:30 +0000

    fixing cart

--- src/main/java/com/example/springbootproject/ProjectApplication.java
+++ src/main/java/com/example/springbootproject/ProjectApplication.java
@@ -14,12 +14,11 @@ public class ProjectApplication {
         // SignUp.logUser("user5@something.com", "password5");
         // Login.validateLogin("user1@something.com", "password1");
         // Cart.AddToCart("Book Title", "Room 1");
-        // Cart.addBook("Book1", "Author1", 4.39);
+        Cart.addBook("Book3", "Author3", 4.39);
         // Cart.addRoom("Room1", 5, 4.39);
         // Book.addBook("Book1", "Author1", 1.00);
         // Book.removeBook("Book4");
         // Room.addRoom("Room1", 1, true);
         // Room.removeRoom("Room2");
-        SignUp.loginUser("user3@something.com", "password3");
     }
 }
