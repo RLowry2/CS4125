7c2ea41c9e4aeb22706db989afc376cb6818d1f4
18244513 <18244513@studentmail.ul.ie>
2021-12-09 22:31:31 +0000

    Availability done!??

--- src/main/java/com/example/springbootproject/model/RoomRental.java
+++ src/main/java/com/example/springbootproject/model/RoomRental.java
@@ -1,6 +1,16 @@
 package com.example.springbootproject.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
 import javax.persistence.*;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 
 
 @Entity
@@ -14,12 +24,70 @@ public class RoomRental {
         super();
     }
 
-    public long setID() {
+    public long getID() {
         return id;
     }
 
-    public long setAvailablity() {
+    public long getAvailablity() {
         return availability;
+    }
+
+    public static void bookRoom(String room, String day) throws CsvException, IOException {
+		CSVReader csvReader = new CSVReader(new FileReader("Availability.csv"));
+		List<String[]> csvInListFormat = csvReader.readAll();
+		csvInListFormat.forEach(x -> System.out.println(Arrays.toString(x)));		
+
+
+		String csv = "availability.csv";
+        BufferedReader br = null;
+        String line = "";
+        String csvSplit = ",";
+        String[] read = new String[0];
+		int i = 0; 
+        try {
+            br = new BufferedReader(new FileReader(csv));
+            String headerLine = br.readLine();
+
+            while ((line = br.readLine()) != null) {
+				read = line.split(csvSplit);
+				if(read[0].equals(room)) {
+                    switch (day) {
+                        case "Monday": if(read[1].equals(true)) {
+                            read[1] = "false";
+                            }
+                            break;
+                        case "Tuesday": if(read[2].equals(true)) {
+                            read[2] = "false";
+                            }
+                            break;
+                        case "Wednesday": if(read[3].equals(true)) {
+                            read[3] = "false";
+                            }
+                            break;
+                        case "Thursday": if(read[4].equals(true)) {
+                            read[4] = "false";
+                            }
+                            break;
+                        case "Friday": if(read[5].equals(true)) {
+                            read[5] = "false";
+                            }
+                            break;
+                    }
+					break;
+				} else {
+					i++;
+				}
+			}
+        }
+        catch (IOException io) {
+            System.out.println(io);
+        }
+
+		/* csvInListFormat.remove(i+1);
+		FileWriter sw = new FileWriter("availability.csv");
+		CSVWriter writer = new CSVWriter(sw);
+		writer.writeAll(csvInListFormat);
+		writer.close(); */
     }
 
  }
