278f21e2418866e2d7484dfbb8da48cb391b67b3
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2021-12-15 14:21:44 +0000

    Update Cart.java

--- springbootattempt/src/main/java/com/springbootattempt/springbootattempt1/Cart.java
+++ springbootattempt/src/main/java/com/springbootattempt/springbootattempt1/Cart.java
@@ -1,63 +1,16 @@
-package com.springbootattempt.springbootattempt1;
+package com.example.springbootproject.model;
  
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.persistence.*;
+
+import com.opencsv.exceptions.CsvException;
  
 @Entity
 public class Cart extends User {
-    // @Id
-    // @GeneratedValue(strategy=GenerationType.AUTO)
-    // private long bookId;
-    // private String title;
-    // private float bookPrice;
-
-    // private long roomId;
-    // private String roomNumber;
-    // private float roomPrice;
-    public static ArrayList<String> itemsList = new ArrayList<String>();
-    // private float transaction;
-    // private String userType;
-   
-    // public Cart() {
-    //     super();
-    // }
  
-    // public Cart(String title, long bookId,String roomNumber, long roomId) {
-        
-    //     super();
-    //     this.title = title;
-    //     this.bookId = bookId;
-
-    //     this.roomNumber = roomNumber;
-    //     this.roomId = roomId;
-
-    // }
-
-
-    // public String CheckUser(){
-    //     userType = User.userType;
-    //     return userType;
-    // }
-
-    // Needs to be modified users
-    public static void AddToCart(String title, String roomNumber) {   
-        itemsList.add(title);
-        itemsList.add(roomNumber);
-    }
-
-    // public long getbookId() {
-    //     return bookId;
-    // }
- 
-    // public String getTitle() {
-    //     return title;
-    // }
- 
-	public static void addBook(String title, String author, double price) throws IOException {
+	public static void addBook(String title, String author, double price) throws IOException, CsvException {
 		try (FileWriter writer = new FileWriter("cart.csv", true)) {
 
 		StringBuilder sb = new StringBuilder();
@@ -70,15 +23,12 @@ public class Cart extends User {
 
 		writer.write(sb.toString());
 
+        Book.removeBook(title);
+
 		} catch (FileNotFoundException e) {
 		System.out.println(e.getMessage());
 		}
 	}
-
-
-    // public long getRoomId() {
-    //     return roomId;
-    // }
  
 	public static void addRoom(String number, int size, double price) throws IOException {
 		try (FileWriter writer = new FileWriter("cart.csv", true)) {
@@ -96,46 +46,6 @@ public class Cart extends User {
 		} catch (FileNotFoundException e) {
 		System.out.println(e.getMessage());
 		}
-	}
-
-
-    // public float getRoomPrice() {
-    //     return roomPrice;
-    // }
-
-    // public String getRoomNumber() {
-    //     return roomNumber;
-    // }
-
-    // public float Cost(float price, float transaction, float discount){
-
-    //     if(userType == "Student"){
-    //         discount = price * (1/4);
-    //     }
-    //     else if(userType == "Charity"){
-    //         discount = price * (1/2);
-    //     }
-    //     else if(userType == "Admin"){
-    //         discount = price * (1/2);
-    //     }
-    //     else{
-    //         discount = 0;
-    //     }
-
-    //     transaction = price - discount;
-    //     return transaction;
-
-    // }
-
-
-    // public ArrayList<String> getItemsList() {
-    //     return itemsList;
-    // }
- 
-    // public void setItems(ArrayList<String> itemsList) {
-    //     this.itemsList = itemsList;
-    // }
-
- 
+	} 
 }
  
